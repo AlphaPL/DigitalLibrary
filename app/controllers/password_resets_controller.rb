46391525b0c6dcaddea9d45057695e64db473fe0
Arek Krawczyk <arkadiusz.krawczyk@nsn.com>
2015-01-24 14:29:39 +0100

    Added new heroku relates stuff.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -56,7 +56,7 @@ class PasswordResetsController < ApplicationController
 
     # Confirms a valid user.
     def valid_user
-      puts @user
+      puts @user, @user.activated?, @user.authenticated?(:reset, params[:id])
       unless (@user && @user.activated? &&
               @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
